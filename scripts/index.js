ba9f6dcebc271111dd99f38a741b686a38a5d598
mauricio <mauris_amigos@hotmail.com>
2020-09-02 13:37:33 -0500

    Boton completado

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 const d = document;
-const $button = d.querySelector(".button-container");
-const $footer = d.querySelector(".footer-social");
+const $button = d.querySelector(".button-container"),
+     $footer = d.querySelector(".footer-social");
 
 const desplegarmenu = ()=>{
-     $footer.classList.toggle('is-active')
+     $footer.classList.toggle('is-active');
+     $button.classList.toggle('is-active');
 }
 
 $button.addEventListener('click',desplegarmenu);
